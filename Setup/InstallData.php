7ac99b7c6ef6f98f4aa7a5d9f4b94bb188800b94
cuongdoan17 <cuongdoan1712@gmail.com>
2021-01-12 16:47:37 +0700

    show custom category attribute in menu

---
+++ Setup/InstallData.php
@@ -0,0 +1,80 @@
+<?php
+namespace AHT\CateAtrribute\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements \Magento\Framework\Setup\InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        /**
+         * Insert/Create a simple text attribute
+         */
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'thumbnail',
+            [
+                'type' => 'varchar',
+                'label' => 'Thumbnail',
+                'input' => 'image',
+                'backend' => 'Magento\Catalog\Model\Category\Attribute\Backend\Image',
+                'required' => false,
+                'sort_order' => 9,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+            ]
+        );
+
+        /**
+         * Insert/Create a seletbox attribute with custom options
+         */
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'is_landing',
+            [
+                'type' => 'int',
+                'label' => 'Is Landing',
+                'input' => 'select',
+                'sort_order' => 333,
+                'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                'global' => 1,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => null,
+                'backend' => ''
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}
